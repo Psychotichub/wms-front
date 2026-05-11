f996db5851f70d67179eb30046e718d91fbd55d1
Psychotichub <4psychotic@gmail.com>
2026-05-11 13:59:57 +0300

    vupdte

--- app.config.js
+++ app.config.js
@@ -3,6 +3,9 @@ const apiUrl = process.env.EXPO_PUBLIC_API_URL ||
   process.env.EXPO_PUBLIC_API_URL_DEV ||
   null;
 
+const adminSignupCode = process.env.EXPO_PUBLIC_ADMIN_SIGNUP_CODE || null;
+const webPushVapidPublicKey = process.env.EXPO_PUBLIC_WEB_PUSH_VAPID_PUBLIC_KEY || null;
+
 module.exports = {
   expo: {
     name: 'WMS',
@@ -32,6 +35,8 @@ module.exports = {
     runtimeVersion: '1.0.0',
     extra: {
       apiUrl,
+      adminSignupCode,
+      webPushVapidPublicKey,
       eas: {
         projectId: 'd1f4d263-2161-4968-a6eb-0a7f8de630f7'
       }
